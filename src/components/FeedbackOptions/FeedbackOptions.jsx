0e54c0a12cb9c968f78b03ebe371ff167f5720c0
lewypopescu <popesculewy@gmail.com>
2024-07-06 10:58:10 +0300

    first done

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './FeedbackOptions.module.css';
+
+class FeedbackOptions extends Component {
+  render() {
+    const { options, onLeaveFeedback } = this.props;
+    return (
+      <div>
+        {options.map(option => (
+          <button key={option} onClick={() => onLeaveFeedback(option)}>
+            {option.charAt(0).toUpperCase() + option.slice(1)}
+          </button>
+        ))}
+      </div>
+    );
+  }
+}
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
